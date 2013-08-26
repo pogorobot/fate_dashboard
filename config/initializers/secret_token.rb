9bca5f5b57a784d9c2dc5c43d9b0e70ebb0ca120
Josh Eklund <joshe@hranswerlink.com>
2013-08-25 18:12:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FateDashboard::Application.config.secret_token = '70d9cf4531be6a7e8810b7792975675713685e222c4fa6a6868697baedee6c0641b6fbccf7b4a705e9ea5e5fbc0d0ba177d2225127bb0d981f4a2014af431e52'
